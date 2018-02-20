a28ff66eae707a2bb83c84fbbee74ea9e56358fe
Maryna Delidova <marynadelidova@marynas-macbook-pro.local>
2018-02-20 21:30:34 +0200

    Initial Commit

---
+++ Methods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Methods
+//
+//  Created by Maryna Delidova on 2/20/18.
+//  Copyright © 2018 Maryna Delidova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
